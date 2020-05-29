8175b571f3b4e1c4bc28f7ba9bcd14f1f821ada4
DouliDz <khirodouli@gmail.com>
2020-05-29 15:45:40 +0100

    kata#14 solution.

--- kata/Recover_a_secret_string_from_random_triplets.py
+++ kata/Recover_a_secret_string_from_random_triplets.py
@@ -22,5 +22,54 @@ triplets = [
   ['w','h','s']
 ]
 """
+
 def recoverSecret(triplets):
-  'triplets is a list of triplets from the secrent string. Return the string.'
+  secret=[]
+  for a in triplets:
+      x = a[0]
+      y = a[1]
+      z = a[2]
+      if x not in secret:
+          secret.insert(0,x)
+      if y not in secret:
+          secret.insert(secret.index(x)+1,y)
+      if y in secret and secret.index(y) < secret.index(x):
+          secret.pop(secret.index(y))
+          secret.insert(secret.index(x) + 1, y)
+      if z not in secret:
+          secret.insert(secret.index(y)+1,z)
+      if z in secret and secret.index(z) < secret.index(y):
+          secret.pop(secret.index(z))
+          secret.insert(secret.index(y) + 1, z)
+  return ''.join(secret)
+
+"""
+--- best solutions:
+from collections import defaultdict
+
+def recoverSecret(triplets):
+    letters = defaultdict(set)
+    for a, b, c in triplets:
+        letters[a].add(b)
+        letters[a].add(c)
+        letters[b].add(c)
+
+    for key, value in letters.items():
+        for after_key in value:
+            letters[key] = letters[key].union(letters[after_key])
+
+    return ''.join(k for k, _ in sorted(
+        letters.iteritems(), key=lambda (_, v): len(v), reverse=True))
+
+
+def recoverSecret(triplets):
+  s = list(set([c for t in triplets for c in t]))
+  for t in triplets:
+    if s.index(t[1]) > s.index(t[2]):
+      s.remove(t[1])
+      s.insert(s.index(t[2]), t[1])
+    if s.index(t[0]) > s.index(t[1]):
+      s.remove(t[0])
+      s.insert(s.index(t[1]), t[0])
+  return ''.join(s)
+"""
